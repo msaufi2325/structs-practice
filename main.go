34964bde4ebde97460eaaa212179a9892564f8cf
Saufi <msaufi2325@gmail.com>
2023-12-28 21:20:38 +0900

    Sec 5: 90. Practice Project: Creating a Struct & Constructor Function

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"example.com/note/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
+
+	content := getUserInput("Note content: ")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	var value string
+	fmt.Scanln(&value)
+
+	return value
+}
